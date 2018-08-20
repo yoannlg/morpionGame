6e0083efca9ba2eff98d232d6a665a9e815ecde4
yoannlg <shinji51@hotmail.fr>
2018-08-20 16:02:14 +0200

    add condition and message of victory (not clean for the moment) and refresh the page for replay a new game

--- js/morpion.js
+++ js/morpion.js
@@ -9,31 +9,85 @@ let stop = false;
 const tour = () => {
     return coup % 2 === 0 ? 'circle' : 'cross';
 }
-const isWin = (elem) =>{
-    for (let i = 0; i < casesArray.length; i++) {
-
-        let classTest = casesArray[i].classList.contains('circle');
-
-    //    if(casesArray[0].classList.contains('circle')&&
-    //     casesArray[1].classList.contains('circle')&&
-    //     casesArray[2].classList.contains('circle')){
-            
-    //    };
 
 
-//     switch (???) {
-//         case casesArray[0].classList.contains('circle'):
-//         console.log("TEEEEEESSSSST");
-//             break;
-
-//         default:
-//             break;
-//         }
+//fonctions is circle et is cross qui vérifie si les lignes horizontale, verticale et diagonale sont remplie par des rond ou des croix
+const isCircle = ()=>{
+    if(casesArray[0].classList.contains('circle')&&
+    casesArray[1].classList.contains('circle')&&
+    casesArray[2].classList.contains('circle') ||
+        casesArray[3].classList.contains('circle')&&
+        casesArray[4].classList.contains('circle')&&
+        casesArray[5].classList.contains('circle') ||
+    casesArray[6].classList.contains('circle')&&
+    casesArray[7].classList.contains('circle')&&
+    casesArray[8].classList.contains('circle') ||
+        casesArray[0].classList.contains('circle')&&
+        casesArray[3].classList.contains('circle')&&
+        casesArray[6].classList.contains('circle') ||
+    casesArray[1].classList.contains('circle')&&
+    casesArray[4].classList.contains('circle')&&
+    casesArray[7].classList.contains('circle') ||
+        casesArray[2].classList.contains('circle')&&
+        casesArray[5].classList.contains('circle')&&
+        casesArray[8].classList.contains('circle') ||
+    casesArray[0].classList.contains('circle')&&
+    casesArray[4].classList.contains('circle')&&
+    casesArray[8].classList.contains('circle') ||
+        casesArray[2].classList.contains('circle')&&
+        casesArray[4].classList.contains('circle')&&
+        casesArray[6].classList.contains('circle')){
+        return true;
     }
-               
+}
 
-    if(elem.classList.contains('circle')){
-        return stop = true;
+const isCross = ()=>{
+    if(casesArray[0].classList.contains('cross')&&
+    casesArray[1].classList.contains('cross')&&
+    casesArray[2].classList.contains('cross') ||
+        casesArray[3].classList.contains('cross')&&
+        casesArray[4].classList.contains('cross')&&
+        casesArray[5].classList.contains('cross') ||
+    casesArray[6].classList.contains('cross')&&
+    casesArray[7].classList.contains('cross')&&
+    casesArray[8].classList.contains('cross') ||
+        casesArray[0].classList.contains('cross')&&
+        casesArray[3].classList.contains('cross')&&
+        casesArray[6].classList.contains('cross') ||
+    casesArray[1].classList.contains('cross')&&
+    casesArray[4].classList.contains('cross')&&
+    casesArray[7].classList.contains('cross') ||
+        casesArray[2].classList.contains('cross')&&
+        casesArray[5].classList.contains('cross')&&
+        casesArray[8].classList.contains('cross') ||
+    casesArray[0].classList.contains('cross')&&
+    casesArray[4].classList.contains('cross')&&
+    casesArray[8].classList.contains('cross') ||
+        casesArray[2].classList.contains('cross')&&
+        casesArray[4].classList.contains('cross')&&
+        casesArray[6].classList.contains('cross')){
+        return true;
+    }
+}
+
+
+
+
+
+const isWin = () =>{
+    for (let i = 0; i < casesArray.length; i++) {
+       if(isCircle() || isCross()){
+           return true;
+       };
+
+    // switch (???) {
+    //     case casesArray[0].classList.contains('circle'):
+    //     console.log("TEEEEEESSSSST");
+    //         break;
+
+    //     default:
+    //         break;
+    //     }
     }
 }
 
@@ -41,11 +95,21 @@ const isWin = (elem) =>{
 const play = (e) =>{
     console.log("Evenement vaut : "+e);
     const element = e.target;
-     if(!element.classList.contains('circle'||'cross')){
+
+     if(!element.classList.contains('circle'||'cross') && !isWin()){
         element.classList.add(tour());
-        isWin(element);
         coup++;
      }
+     //timeOut qui permet d'afficher l'alert apres l'affichage du rond ou de la croix sans ça l'alert apparait avant l'affichage du dernier rond ou croix permetant la victoire
+     setTimeout(()=>{
+        if(isWin()){
+            console.log("c'est gagner et ouai ouai");
+            alert("BRAVO JEANNOT");
+            window.location.reload();
+        }
+     },500);
+     
+
 }
 
 cases.forEach(elem => elem.addEventListener('click', play));
